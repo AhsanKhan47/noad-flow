42a880d758ad84dbdafa302cc6ac63544a8dcd29
Ahsankhan47 <ahsankhanafridi2000@gmail.com>
2023-05-25 04:47:20 +0500

    import statement removed to avoid build error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { GiHamburgerMenu } from "react-icons/Gi";
-import { AiOutlineClose } from "react-icons/Ai";
+
 import { Menu, MenuClose } from "./shared/icons";
 import Link from "next/link";
 import { useState } from "react";
